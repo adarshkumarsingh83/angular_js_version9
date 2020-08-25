10f9f7c0d0822f1e8e791df9001388948ded1b32
adarsh <us-guest@USMB113823-2.local>
2020-08-25 14:26:43 -0500

    angular-reactivefrom-setValue-getValue-resetValue

---
+++ APPLICATIONS/angular-reactivefrom-setValue-getValue-resetValue/README.md
@@ -0,0 +1,15 @@
+# AngularReactivefromSetValueGetValueResetValue
+
+### Appliation Generation 
+* $ ng new angular-reactivefrom-setValue-getValue-resetValue
+```
+? Would you like to add Angular routing? Yes
+? Which stylesheet format would you like to use? 
+  CSS 
+❯ SCSS   [ https://sass-lang.com/documentation/syntax#scss                ] 
+  Sass   [ https://sass-lang.com/documentation/syntax#the-indented-syntax ] 
+  Less   [ http://lesscss.org                                             ] 
+  Stylus [ http://stylus-lang.com                                         ] 
+```
+
+### 
---
+++ APPLICATIONS/angular-reactivefrom-setValue-getValue-resetValue/src/app/form/form.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'; 
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  //same form name as tempate form name
+  myForm: FormGroup; 
+
+  constructor(private formBuilder: FormBuilder){
+    this.myForm = formBuilder.group({
+      nameField: ['', Validators.required],
+      emailField: ['', Validators.required],
+      pwdField: ['', Validators.required],
+      termField: ['', Validators.requiredTrue],
+    })
+  }
+  
+  data;
+  postData(){
+    console.log(this.myForm);
+    console.log(this.myForm.value);
+    console.log(this.myForm.value.nameField);
+    console.log(this.myForm.value.emailField);
+    console.log(this.myForm.value.pwdField);
+    console.log(this.myForm.value.termField);
+    this.data = { "name": this.myForm.value.nameField,"email": this.myForm.value.emailField,"pwd": this.myForm.value.pwdField, "term": this.myForm.value.termField}
+    console.log(this.data);
+ }
+
+  ngOnInit(): void {
+    
+    // for complete data set 
+    this.myForm.setValue(
+      {
+        nameField: 'adarsh kumar',
+         emailField: 'adarsh@kumar',
+         pwdField: 'xxxx',
+         termField: false
+       }
+    );
+
+    // for partieal data set 
+    this.myForm.patchValue(
+      {
+         emailField: 'adarsh@kumar',
+         pwdField: 'yyyy'
+       }
+    );
+  }
+
+  resetForm(){
+    this.myForm.reset();  
+  }
+}
